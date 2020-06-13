d83c1112dea550e0d9f76243b4320bad8de9528d
eight <f@tpeop.le>
2020-06-13 18:44:23 -0400

    fixed makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
-32BIT = -m32
+32BIT = -m32 -no-pie
 GNU = -D_GNU_SOURCE
-FLAGS = -Wall -Wextra
+FLAGS = -Wall -Wextra -Wl,-Ttext-segment=0x20000000
 LINKS = -ldl
 SRCS = elph.c loder.c helper.c sym.c
 OUT = elph
--- elph.c
+++ elph.c
@@ -90,7 +90,8 @@ void elph_relocate(char *buf, int off, int shsize,
 
 		elph_debug("REL:\taddr=0x%x name=\"%s\" dsym=0x%x", (int)addr, name, (int)dsym);
 
-		if(!dsym) dsym = (Elf32_Addr*)&elph_unknown;
+		if(!dsym)
+			dsym = (Elf32_Addr*)&elph_unknown;
 
 		switch(ELF32_R_TYPE(rel[i].r_info)) {
 		case R_386_GLOB_DAT: *(Elf32_Addr*)addr = (int)dsym; break;
